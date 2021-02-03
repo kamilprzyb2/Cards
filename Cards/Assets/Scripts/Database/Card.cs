a5c23f4cf7f13666768c667549737af2d6195430
Kamil <kprzybylski2@edu.cdv.pl>
2021-02-03 21:37:25 +0100

    Minor UI tweaks

--- Cards/Assets/Scripts/Database/Card.cs
+++ Cards/Assets/Scripts/Database/Card.cs
@@ -23,4 +23,41 @@ public class Card
         Note = model.Note;
     }
 
+    /// <summary>
+    /// returns special 404 card, to use when you don't have any cards left
+    /// </summary>
+    /// <returns></returns>
+    public static Card card404()
+    {
+        CardModel model = new CardModel();
+        model.Id = -1;
+        model.GroupId = -1;
+        model.Name = "Card 404";
+        model.Note = "In theory player should never see this";
+        model.Type = 404;
+        model.Decisions = new Decision[2];
+        model.Decisions[0] = new Decision();
+        model.Decisions[1] = new Decision();
+        model.Decisions[0].Description = "I'll be honest, I have no idea how you got there. This part of the story isn't even finished yet!";
+        model.Decisions[1].Description = "Can we pretend this never happend and start over?";
+        model.Decisions[0].Name = "Restart";
+        model.Decisions[1].Name = "Restart";
+        model.Decisions[0].GroupId = -1;
+        model.Decisions[1].GroupId = -1;
+        model.Decisions[0].Values = new int[4];
+        model.Decisions[1].Values = new int[4];
+
+        model.Decisions[0].IconId = 1;
+        model.Decisions[1].IconId = 1;
+
+        for (int i = 0; i < 2; i++)
+        {
+            for (int j =0; j < 4; j++)
+            {
+                model.Decisions[i].Values[j] = 0;
+            }
+        }
+
+        return new Card(model);
+    }
 }
